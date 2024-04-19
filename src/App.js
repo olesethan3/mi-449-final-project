a1859deb6c52a88cf4fd86cf516bdcbbb9d39cce
Ethan Oles <oleseth1@msu.edu>
2024-04-19 17:50:27 -0400

    Site Now Shows All Details for Each Dorm Will Implement Tailwind CSS Next

--- src/App.js
+++ src/App.js
@@ -16,27 +16,32 @@ function Dorms() {
   }, []);
 
   return (
-    <table>
-      <tbody>
-        {dormInfo.map(d => (
-          <tr key={d.id}>
-            <td>{d.name}</td>
-            <td>{d.neighborhood}</td>
-            <td>{d.year}</td>
-          </tr>
+    <div>
+        {dormInfo
+        .filter(d => [1].includes(d.id))
+        .map(d => (
+          <>
+          <h1>{d.name}</h1>
+          <img src={d.dorm_image} alt={d.name}/>
+          <p>Address: {d.address}</p>
+          <p>Year Opened: {d.year}</p>
+          <p>Neighborhood: {d.neighborhood}</p>
+          <p>Number of Floors: {d.floors}</p>
+          <p>Bathoom Style: {d.bathroom_style}</p>
+          <p>Double Rooms Available?: {d.double_rooms}</p>
+          <p>Single Rooms Available?: {d.single_rooms}</p>
+          <p>Dining Hall Inside?: {d.dining_hall}</p>
+          </>
         ))}
-      </tbody>
-    </table>
-  );
+    </div>
+  )
 }
-
-function App() {
-  return (
-    <>
-      <p>Hello</p>
-      <Dorms />
-    </>
-  );
-}
-
-export default App;
+  
+  function App() {
+    return (
+      <>
+        <Dorms />
+      </>
+    );
+  }
+  export default App;
